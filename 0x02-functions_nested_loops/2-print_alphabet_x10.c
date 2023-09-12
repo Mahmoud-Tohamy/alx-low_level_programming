6d8c77fea647af4584385baaa90ad3752d54b699
Top Rake <herooftroll99@gmail.com>
2023-09-12 09:59:24 -0700

    asdasd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description: This function prints the alphabet, in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	int ch, f;
+
+	for (f = 0; f < 10; f++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
