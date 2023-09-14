ac745ca8c5ec53f2582a0f58a003f442f8187eec
Top Rake <herooftroll99@gmail.com>
2023-09-14 13:47:05 -0700

    asdasd

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Descripation: prints the numbers from 1 to 100
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	i++;
+	}
+return (0);
+}
