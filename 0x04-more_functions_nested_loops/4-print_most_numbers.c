5343d61cd78e9ed0d3f9c4a160cc0887b0af86fb
Top Rake <herooftroll99@gmail.com>
2023-09-14 12:10:18 -0700

    sadad

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,9 @@
 
 /**
  *print_most_numbers - prints numbers except 2 and 4
- *
- *Return: returns nothing
+ *		  from 2 and 4 and you can
+ *		  only use _putchar twice
+ *Return: Always 0 (success)
 */
 
 void print_most_numbers(void)
