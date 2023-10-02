3758410d26459f40c10553b588fd7983ef65fa55
Top Rake <herooftroll99@gmail.com>
2023-10-02 08:30:27 -0700

    asdasd

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,15 +8,13 @@
  * Return: 0
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc--)
-	{
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-		i++
-	}
 
 	return (0);
 }
+
