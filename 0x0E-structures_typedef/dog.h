b7b502c7b3d009dd68d555e7120ad5f8643f3ef0
Top Rake <herooftroll99@gmail.com>
2023-10-10 16:53:36 -0700

    asdas

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOH_H
+#define DOH_H
+
+/**
+* struct dog - THE DOG STRUCT
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Description: a lone dog struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOH_H */
