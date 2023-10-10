e025e5fa35b5f443a4d32de4b8d597e77c0f6505
Top Rake <herooftroll99@gmail.com>
2023-10-10 07:13:29 -0700

    asdasd

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTIOM_LIKE_MACRO_H
 #define _FUNCTIOM_LIKE_MACRO_H
 
-#define ABS(x) (((x) < (0 ? (-(x)) : (x))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif /* _FUNCTIOM_LIKE_MACRO_H */
