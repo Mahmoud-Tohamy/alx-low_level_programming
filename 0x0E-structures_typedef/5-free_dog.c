6ec51ab96ae61e0d60e548c4c00c337f9cd6e5ff
Top Rake <herooftroll99@gmail.com>
2023-10-10 17:40:46 -0700

    asdsasd

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dags
+ * @d: dag
+ * Return: viod
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
