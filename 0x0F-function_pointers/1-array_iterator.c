85b46cd5d480aa515db1803e1db12dc1e8ef305c
Top Rake <herooftroll99@gmail.com>
2023-10-13 06:34:41 -0700

    asdasd

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,13 +6,13 @@
  * @size: size of array.
  * @action: pointer to the function.
  *
- * Return: nothing
+ * Return: Void.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int *end = array + size - 1;
 
-	if (array && action && action)
-		while (array < end)
+	if (array && size && action)
+		while (array <= end)
 			action(*array++);
 }
