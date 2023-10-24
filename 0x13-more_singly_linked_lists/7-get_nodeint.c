18719435290d084e0d7933ce1cc4a256fa8669cb
Top Rake <herooftroll99@gmail.com>
2023-10-24 09:28:32 -0700

    asdasd

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - returns node at index
+* @head: pointer to the head of the list
+* @index: index of the node required
+* Return: the address of the node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node
+	unsigned int n;
+
+	for (node = head, n = 0; node && n < index; node = node->next, n++)
+		;
+	return (node);
+}
