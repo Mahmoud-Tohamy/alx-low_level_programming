1aa12cb2fd6c76b2566d4da9119ac94febbe230b
Top Rake <herooftroll99@gmail.com>
2023-10-26 14:41:39 -0700

    asd

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "mani.h"
+/**
+* get_endianness - return the endianness of system
+* Return: 0 if big endian, 1 if small
+*/
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+
+	return (*(char *)&n);
+}
