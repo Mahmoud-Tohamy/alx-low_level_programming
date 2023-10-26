2dc10edff43b0fd5eb7d5b524def7b3b27f55df3
Top Rake <herooftroll99@gmail.com>
2023-10-26 13:16:30 -0700

    asdasd

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_binary - print an num as binary string
+* @n: nom to print
+* Return: Void
+*/
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, printed = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
+}
